6c398a6e6248e7ea07d7fb94da67402d6b173468
mon1210 <himejijin@i.softbank.jp>
2023-11-23 18:45:01 +0900

    add Model.cppの追加

---
+++ My3DAction/Model.cpp
@@ -0,0 +1,35 @@
+/*
+* @file	 Model.h
+* @brief
+*
+*
+*/
+#include "DxLib.h"
+#include "Constants.h"
+#include "Model.h"
+
+
+// 
+Model::Model()
+{
+    enemyHandle = 0;
+    playerHandle = 0;
+
+}
+
+
+// 
+Model::~Model()
+{
+}
+
+
+// 
+void Model::ModelSet()
+{
+    // ÇRÇcÉÇÉfÉãÇÃì«Ç›çûÇ›
+    enemyHandle = MV1LoadModel("res\\Enemy\\EnemyModel.mv1");
+    // 
+    playerHandle = MV1LoadModel("res\Player\PlayerModel.mv1");
+
+}
