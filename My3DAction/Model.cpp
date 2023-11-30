ef2c0a4508affc94de4f93c5c576f7ae92cf470f
mon1210 <himejijin@i.softbank.jp>
2023-11-30 23:02:42 +0900

    update それぞれのモデル取得用関数とゲッターの作成

--- My3DAction/Model.cpp
+++ My3DAction/Model.cpp
@@ -12,6 +12,7 @@ Model::Model()
 {
     enemyHandle = 0;
     playerHandle = 0;
+    tileHandle = 0;
 
 }
 
@@ -25,7 +26,7 @@ Model::~Model()
 /**
 * @brief プレイヤーモデル読み込み関数
 */
-void Model::PlayerLoadModel()
+void Model::LoadPlayerModel()
 {
     // 3Dモデルの読み込み
     playerHandle = MV1LoadModel("res\\Player\\PlayerModel2.mv1");
@@ -33,10 +34,47 @@ void Model::PlayerLoadModel()
 
 
 /**
-* @brief  エネミーモデル読み込み関数
+* @brief エネミーモデル読み込み関数
 */
-void Model::EnemyLoadModel()
+void Model::LoadEnemyModel()
 {
     // 3Dモデルの読み込み
     enemyHandle = MV1LoadModel("res\\Enemy\\EnemyModel2.mv1");
+}
+
+
+/**
+* @brief タイルモデル読み込み関数
+*/
+void Model::LoadTileModel()
+{
+    // 3Dモデルの読み込み
+    tileHandle = MV1LoadModel("res\\test_Tile_model.mv1");
+ }
+
+
+/**
+* @brief プレイヤーモデル取得用関数
+*/
+int Model::GetPlayerModel()
+{
+    return playerHandle;
+}
+
+
+/**
+* @brief エネミーモデル取得用関数
+*/
+int Model::GetEnemyModel()
+{
+    return enemyHandle;
+}
+
+
+/**
+* @brief タイルモデル取得用関数
+*/
+int Model::GetTileModel()
+{
+    return tileHandle;
 }
