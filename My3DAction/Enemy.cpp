1fe5bd73bf867f0afe81b039d8b3a717bfad14e7
mon1210 <himejijin@i.softbank.jp>
2023-12-03 00:51:34 +0900

    add 行動状態によって動作を変更、待機=>移動=>待機のループを実装

--- My3DAction/Enemy.cpp
+++ My3DAction/Enemy.cpp
@@ -1,7 +1,9 @@
+#include <math.h>
 #include "Enemy.h"
 #include "Constants.h"
 #include "Model.h"
 #include "Enums.h"
+#include "BG.h"
 
 
 /**
@@ -16,12 +18,16 @@ Enemy::Enemy()
     hitPoint = 1.f;
     animTimer = 0.f;
     pModel = NULL;
+    pBG = NULL;
     angle = ENEMY_START_ROTATE_Y;
 
     position = VGet(ENEMY_START_POS_X, ENEMY_START_POS_Y, ENEMY_START_POS_Z);
 
     animTime = MV1GetAnimTotalTime(animHandle, 0);
 
+    currentState = EnemyState::Wait;
+
+    pBG = new BG();
     pModel = new Model();
 
     // モデル取得
@@ -38,7 +44,164 @@ Enemy::Enemy()
 // デストラクタ
 Enemy::~Enemy()
 {
+    SAFE_DELETE(pBG);
     SAFE_DELETE(pModel);
+}
+
+
+/**
+* @brief アニメーション状態初期化関数
+* @note  My3DAction.cppで一回だけ呼び出す
+*/
+void Enemy::InitAnimation()
+{
+    SetAnim(eEnemy::Idle);
+}
+
+
+/**
+* @brief 行動状態の管理メソッド
+* @note  毎フレームの処理
+*/
+void Enemy::Update()
+{
+    switch (currentState)
+    {
+    case EnemyState::Wait:
+        Wait();
+        break;
+    case EnemyState::Move:
+        Move();
+        break;
+    case EnemyState::Chase:
+        Chase();
+        break;
+    //case EnemyState::Attack:
+    //    break;
+    //case EnemyState::Damage:
+    //    break;
+    //case EnemyState::Death:
+    //    break;
+    }
+
+    // Rader
+
+
+    // 描画メソッド呼び出し
+    draw();
+}
+
+
+/**
+* @brief Wait状態の管理メソッド
+* 
+*/
+void Enemy::Wait()
+{
+    // 行動 => 何もしない
+    SetAnim(eEnemy::Idle);  // これがないとanimTimerが増加せず、最初固まる
+
+    // 遷移
+    /*
+        移動：
+            条件：一定時間の経過
+        追跡：
+            条件：視野にプレイヤーが入る
+    */
+    count++;
+
+    if (count > TIME_TO_TRANSITION)   // 遷移タイマーを超えたのでMoveへ
+    {
+        count = 0;
+        currentState = EnemyState::Move;
+        angle = (rand() % 360);  // ランダムな角度を取得
+        SetAnim(eEnemy::Run);
+
+    }
+    // 視野に入っていたら追跡
+    else if (IsTargetVisible() == true)
+    {
+        currentState = EnemyState::Chase;
+        SetAnim(eEnemy::Run);
+    }
+
+}
+
+
+// 
+void Enemy::Move()
+{
+    // 行動：まっすぐ進む(ステージ上の時)
+    // ベクトル算出
+    float rad = angle * DX_PI_F / 180.f;
+    // 三次元ベクトルの生成
+    VECTOR vec = VGet(sinf(rad), 0.f, cosf(rad));
+    // -1を乗算することでベクトルが逆転
+    vec = VScale(vec, -1.f);
+
+    // 座標変更
+    //float move_speed = 0.1f;
+    // ベクトルの大きさを乗算、移動速度に
+    vec = VScale(vec, ENEMY_MOVE_SPEED);
+    // 移動先までのベクトル取得
+    VECTOR new_pos = VAdd(vec, position);
+
+    new_pos.y += 1.0f;  // これがないと左右,下に移動できない
+    // MV1_COLL_RESULT_POLY => 当たり判定の結果情報が保存された構造体
+    MV1_COLL_RESULT_POLY result = MV1CollCheck_Line(
+        pBG->GetModelHandle(),				    // 判定対象となるモデルのフレーム
+        -1,												// 対象となるフレーム番号
+        new_pos,										// Rayの始点   モデルの足元
+        VGet(new_pos.x, new_pos.y - 250.f, new_pos.z)	// Rayの終点   モデルの頭上
+    );
+
+    if (result.HitFlag == 1) // 当たりチェック
+    {
+        // HitPosition => 交点の座標
+        new_pos.y = result.HitPosition.y;
+        position = new_pos;
+        if (IsTargetVisible() == true)
+        {
+            currentState = EnemyState::Chase;
+        }
+    }
+
+    // 遷移
+    /*
+        待機：
+            条件：一定時間の経過
+        追跡：
+            条件：視野にプレイヤーが入る
+    */
+    count++;
+
+    if (count >= TIME_TO_TRANSITION)     // 遷移タイマーを超えたのでWaitへ
+    {
+        count = 0;
+        currentState = EnemyState::Wait;
+        SetAnim(eEnemy::Idle);
+    }
+    // 視野に入っていたら追跡
+    else if (IsTargetVisible() == true)
+    {
+        currentState = EnemyState::Chase;
+        // アニメーションはすでにRunなので変更なし
+    }
+
+}
+
+
+// 
+void Enemy::Chase()
+{
+
+}
+
+
+// 
+bool Enemy::IsTargetVisible()
+{
+    return false;
 }
 
 
@@ -137,7 +300,7 @@ bool Enemy::move()
 void Enemy::draw()
 {
     // 行動管理関数呼び出し
-    SetMove();
+    //SetMove();
 
     animTimer += ENEMY_ANIM_F_INCREMENT;
     // アニメーション時間を過ぎたらリセット
--- My3DAction/Enemy.h
+++ My3DAction/Enemy.h
@@ -6,35 +6,42 @@
 #pragma once
 #include "DxLib.h"
 #include "Enums.h"
-
-//--- ヘッダガードを使用して二度インクルードするのを防ぐ
-#ifndef __IGAMEOBJECT_H__
 #include "IGameObject.h"
-#endif
 
-class Stage;
 class Model;
+class BG;
 
 class Enemy : public IGameObject
 {
 public:
 	Enemy();
 	~Enemy();
+	virtual void InitAnimation();
+	virtual void Update();
+	virtual bool IsTargetVisible();
 	virtual bool move() override;	// エネミーのアニメーションメソッド　true:生存 / false:死亡
 	virtual void draw() override;	// 描画メソッド
 	virtual void SetMove();		// 行動管理関数
 	virtual void SetAnim(eEnemy::AnimationNum num);		// アニメーションセット関数
 	virtual VECTOR GetEnemyPos();	// 座標取得関数
+private:
+	virtual void Wait();
+	virtual void Move();
+	virtual void Chase();
 protected:
 	Model*	pModel;
+	BG*		pBG;
 	VECTOR	position;
 	int		animHandle;
 	int		animNo;
+	int		count;		// フレーム計測用　行動遷移, で使用 
 	float	animTime;
 	float	animTimer;
 	float	angle;
 	float	hitPoint;
 
+	EnemyState currentState;
 private:
+
 
 };
