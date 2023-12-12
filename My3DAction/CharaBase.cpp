0f9fa2ebb8acea5865c36e792737dd8f220234b5
mon1210 <himejijin@i.softbank.jp>
2023-12-12 22:40:56 +0900

    add CharaBaseクラスの作成　PlayerとEnemyの基底クラス

---
+++ My3DAction/CharaBase.cpp
@@ -0,0 +1,16 @@
+/**
+* @file CharaBase.cpp
+* @brief クラス CharaBase の実装
+*/
+
+// Windowsアプリケーション開発用の共通ヘッダファイル
+#include "stdafx.h"
+#include "CharaBase.h"
+
+/**
+* @brief 純粋仮想デストラクタの実体
+* @details デストラクタだけは用意しなければ、サブクラスがデストラクタを呼べない
+*/
+CharaBase::~CharaBase()
+{
+}
---
+++ My3DAction/CharaBase.h
@@ -0,0 +1,28 @@
+/**
+* @file		CharaBase.h
+* @brief	クラスCharaBaseの宣言ファイル
+* @note		Characterの基底クラスで、PlayerとEnemyが継承している
+*			基底クラスでは、共通する関数と変数をまとめて宣言
+*/
+#pragma once
+#include "DxLib.h"
+
+// 基底クラス
+class CharaBase 
+{
+public:
+	virtual ~CharaBase();
+	virtual bool move() = 0;
+	virtual void draw() = 0;
+	virtual void update() = 0;
+
+protected:
+	VECTOR	position;	// 座標
+	int		animHandle;	// モデルハンドル
+	int		animNo;		// アニメーション番号
+	float	animTime;	// アニメーションの総再生時間
+	float	animTimer;	// アニメーションの現在の再生時間
+	float	angle;		// 向き		アタッチ時にradに変換
+	float	hitPoint;	// HP
+	//float damage
+};
