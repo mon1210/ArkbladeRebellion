66fbd54feb1a84a98c94148b07aa5d3cd03e91e2
mon1210 <himejijin@i.softbank.jp>
2023-12-19 00:07:15 +0900

    add UIクラスを追加

---
+++ My3DAction/UI.h
@@ -0,0 +1,26 @@
+/**
+* @file		UI.h
+* @brief	クラス UI の宣言
+*/
+#pragma once
+#include "Constants.h"
+#include "Game.h"
+
+class Game;
+
+class UI
+{
+public:
+	UI(Game *Game_);
+	~UI();
+	void move();			// UIの変動用メソッド
+	void draw();			// 描画用メソッド
+private:
+protected:
+	Game	*pGame = NULL;
+	float	hpRatio = 0.f;	// HPバーの表示比率
+
+};
+
+#define GREEN GetColor(0, 255, 0) 
+#define WHITE GetColor(255, 255, 255)
