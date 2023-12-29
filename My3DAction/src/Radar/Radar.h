b274e83d92f8d71d31d1ae1e0a366e75deed85a5
mon1210 <himejijin@i.softbank.jp>
2023-12-29 23:24:47 +0900

    remove 不要なvirtualの削除

--- My3DAction/src/Radar/Radar.h
+++ My3DAction/src/Radar/Radar.h
@@ -11,10 +11,10 @@ class Radar
 public:
 	Radar();
 	~Radar();
-	virtual void draw();															// 描画メソッド
-	virtual void addPoint(float x, float z, eRadar::PointType type);				// Pointを追加するメソッド
-	virtual void addCenter(float x, float z, float front_vec_x, float front_vec_z);	// Radarの中心物体を設定するメソッド
-	virtual void listReset();														// PointのListを初期化するメソッド	毎フレーム最初に呼び出し
+	void draw();															// 描画メソッド
+	void addPoint(float x, float z, eRadar::PointType type);				// Pointを追加するメソッド
+	void addCenter(float x, float z, float front_vec_x, float front_vec_z);	// Radarの中心物体を設定するメソッド
+	void listReset();														// PointのListを初期化するメソッド	毎フレーム最初に呼び出し
 protected:
 	float	uiPosX = 120.f;				// UIの中心座標X
 	float	uiPosY = 120.f;				// UIの中心座標Y
