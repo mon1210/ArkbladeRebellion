669439a3099aa32a33ad3272e11021d562fbb553
mon1210 <himejijin@i.softbank.jp>
2024-01-13 17:01:41 +0900

    fix Roll中かどうかのflagを作成、無駄なflagを削除

--- My3DAction/src/Player/Player.h
+++ My3DAction/src/Player/Player.h
@@ -49,9 +49,7 @@ protected:
 	float	*withSwordAnimTimes = 0;									// 
 	bool	isMove = false;												// 移動フラグ
 	bool	rollAble = true;											// Roll可能フラグ
-	bool	isFrontRoll = false;										// 前回転中フラグ
-	bool	isRightRoll = false;										// 右回転中フラグ
-	bool	isLeftRoll = false;											// 左回転中フラグ
+	bool	isRoll = false;										// 回転中フラグ
 	bool	isDeath = false;											// 死亡フラグ
 	bool	isAttackAnim = false;										// 
 
