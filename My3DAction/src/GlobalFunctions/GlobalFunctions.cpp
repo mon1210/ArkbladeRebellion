d3ce888ac9400ac3119d789b2c3b370be38d7dc4
mon1210 <himejijin@i.softbank.jp>
2024-01-31 18:53:22 +0900

    add GlobalFunctionに二つのベクトルから外積を求める関数を定義

--- My3DAction/src/GlobalFunctions/GlobalFunctions.cpp
+++ My3DAction/src/GlobalFunctions/GlobalFunctions.cpp
@@ -1,6 +1,11 @@
 #include "GlobalFunctions.h"
 
-// 値を指定された範囲内に収めるメソッド
+/**
+* @brief  値を指定された範囲内に収めるメソッド
+* @param[in] value　範囲内に収める値
+* @param[in] min　	最小値
+* @param[in] max　	最大値
+*/
 int clamp(int value, int min, int max)	
 {
 	if (value < min) 		// 下限
@@ -38,4 +43,22 @@ float clampF(float value, float min, float max)
 
 	}
 
+}
+
+
+/**
+* @brief  二つのベクトルの外積を求めて返す
+* @note	  vec_01とvec_02の外積を求め、VECTOR型を返す
+* @param[in] vec_1　一つ目のベクトル
+* @param[in] vec_2　二つ目のベクトル
+*/
+VECTOR cross(VECTOR vec_01, VECTOR vec_02)
+{
+	VECTOR CrossVec;
+	// 外積計算
+	CrossVec.x = vec_01.y * vec_02.z - vec_01.z * vec_02.y;
+	CrossVec.y = vec_01.z * vec_02.x - vec_01.x * vec_02.z;
+	CrossVec.z = vec_01.x * vec_02.y - vec_01.y * vec_02.x;
+
+	return CrossVec;
 }
