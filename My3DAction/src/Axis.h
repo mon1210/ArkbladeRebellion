4c9a1dfb03b9570e81c169fa472f3b4b00886f7f
mon1210 <himejijin@i.softbank.jp>
2024-01-31 18:39:13 +0900

    add Axis構造体を定義

---
+++ My3DAction/src/Axis.h
@@ -0,0 +1,14 @@
+/**
+* @file		Axis.h
+* @brief	Axis構造体を定めたヘッダファイル
+* @note		OBBの軸保存用
+*/
+#pragma once
+#include "DxLib.h"
+
+struct Axis
+{
+	VECTOR x{ VGet(0.f, 0.f,0.f) };
+	VECTOR y{ VGet(0.f, 0.f,0.f) };
+	VECTOR z{ VGet(0.f, 0.f,0.f) };
+};
