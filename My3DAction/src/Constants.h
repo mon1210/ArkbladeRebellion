c0490c9aab4ddaa1dfc9ec3675edd4bd4c2b0696
mon1210 <himejijin@i.softbank.jp>
2024-03-17 22:58:30 +0900

    update 最大HPをint型に

--- My3DAction/src/Constants.h
+++ My3DAction/src/Constants.h
@@ -83,6 +83,8 @@ const int TIME_TO_TRANSITION = 240;
 const int FULL_CIRCLE_DEGREES = 360;
 //! 左手のフレーム番号
 const int ENEMY_LEFT_HAND_FRAME = 18;
+//! エネミーの最大HP
+const int ENEMY_MAX_HP = 250;
 //! エネミーの初期X座標
 const float ENEMY_START_POS_X = 300.f;
 //! エネミーの初期Y座標
@@ -99,8 +101,6 @@ const float ENEMY_ANIM_F_INCREMENT = 0.35f;
 const float ENEMY_MOVE_SPEED = 5.f;
 //! エネミーの視野円の半径
 const float ENEMY_VIEW_RADIUS = 1500.f;
-//! エネミーの最大HP
-const float ENEMY_MAX_HP = 250.f;
 //! エネミーの体の当たり判定OBBの移動値Y　座標Yに加算する
 const float ENEMY_OBB_TRANS_Y = 250.f;
 //! プレイヤーの攻撃力　エネミークラスで使用するのでここで定義
